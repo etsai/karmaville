b5440743344a7a4b42655f89ccfa3566fe1761d4
Apprentice <apprentice@dbc18.local>
2013-09-23 17:25:51 -0700

    init optimization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :karma_points
 
-  attr_accessible :first_name, :last_name, :email, :username
+  attr_accessible :first_name, :last_name, :email, :username, :total_karma
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
@@ -18,14 +18,17 @@ class User < ActiveRecord::Base
             :uniqueness => {:case_sensitive => false}
 
   def self.by_karma
-    joins(:karma_points).group('users.id').order('SUM(karma_points.value) DESC')
+    self.order('total_karma DESC')
   end
 
-  def total_karma
-    self.karma_points.sum(:value)
-  end
+  # def total_karma
+  #   self.total_karma
+  # end
 
   def full_name
-    "#{first_name} #{last_name}"
+    first_name + " " + last_name
   end
+
+
+
 end
